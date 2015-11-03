a3bb0027971807db982a38e0c6c57f99b462ac20
Denis Markin <markindi@w12-418-22.main.sgu.ru>
2015-11-03 15:16:12 +0300

    Network with one neuron

--- OneNeuron/src/MainClass.java
+++ OneNeuron/src/MainClass.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 /**
  * Created by markindi on 03.11.2015.
  */
 public class MainClass {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите вектор весов в виде {a;b;c}");
+        String respond = scan.nextLine();
+        String[] raw = respond.replaceAll("[{, ,}]", "").split(";");
+        int[] w = new int[raw.length];
+        for (int i = 0; i < raw.length; i++)
+            w[i] = Integer.parseInt(raw[i]);
+        System.out.print("Введите величину сдвига\nn = ");
+        int b = scan.nextInt();
+        Neuron a = new Neuron(w,b);
+        int[]p;
+        scan.nextLine();
+
+        while (!respond.contains("stop")) {
+            System.out.println("Введите входной вектор в виде {a;b;c} либо stop для выхода");
+            if (respond.contains("stop"))
+                break;
+            respond = scan.nextLine();
+            raw = respond.replaceAll("[{, ,}]", "").split(";");
+            p = new int[raw.length];
+            for (int i = 0; i < p.length; i++)
+                p[i] = Integer.parseInt(raw[i]);
+            System.out.println(a.hardlim(p));
+        }
+
+
+    }
 }
--- OneNeuron/src/Neuron.java
+++ OneNeuron/src/Neuron.java
@@ -2,4 +2,26 @@
  * Created by markindi on 03.11.2015.
  */
 public class Neuron {
+    public int[] w;
+    public int b;
+
+    public Neuron(int[] w, int b) {
+        this.w = w;
+        this.b = b;
+    }
+
+    public int sum(int[] p) {
+        int sum = 0;
+        for (int i = 0; i < w.length; i++)
+            sum += w[i] * p[i];
+        for (int i = w.length; i < p.length; i++)
+            sum += p[i];
+        return sum;
+    }
+
+    public int hardlim(int[]p) {
+        if (sum(p)+ b >= 0)
+            return 1;
+        else return 0;
+    }
 }
